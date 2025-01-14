c3eaed2177984cde0d3e10de96fc1d3c7c952560
David Diaz <ctucl-desarrollo@outlook.com>
2025-01-14 12:21:04 -0500

    se cambian algunos pines de la parada del coliseo

--- gpiosManager.py
+++ gpiosManager.py
@@ -5,23 +5,35 @@ class GpiosManager():
         super().__init__()
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
-        self.electroiman = 5
-        self.semaforo = 27
+        self.electroiman = 21
+        self.semaforo = 20
+        self.cruz_roja = 16
+        self.flecha_verde = 12
+        #configuraciones de salidas y entradas
         GPIO.setup(self.electroiman, GPIO.OUT)
         GPIO.setup(self.semaforo, GPIO.OUT)
+        GPIO.setup(self.cruz_roja, GPIO.OUT)
+        GPIO.setup(self.flecha_verde, GPIO.OUT)
         GPIO.setup(self.sensor, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.output(self.semaforo,GPIO.LOW)
-        GPIO.output(self.electroiman,GPIO.HIGH)
+        #valores iniciales
+        GPIO.output(self.semaforo,GPIO.HIGH)
+        GPIO.output(self.flecha_verde, GPIO.HIGH)
+        GPIO.output(self.electroiman,GPIO.LOW)
+        GPIO.output(self.cruz_roja, GPIO.LOW)
 
         
     def doorOpen(self):
-        GPIO.output(self.electroiman, GPIO.LOW)  
-        GPIO.output(self.semaforo, GPIO.HIGH) 
+        GPIO.output(self.electroiman, GPIO.HIGH)  
+        GPIO.output(self.semaforo, GPIO.LOW)
+        GPIO.output(self.flecha_verde, GPIO.LOW)
+        GPIO.output(self.cruz_roja, GPIO.HIGH)
         return "puerta general abierta" 
     
     def doorClose(self):
-        GPIO.output(self.electroiman, GPIO.HIGH)
-        GPIO.output(self.semaforo, GPIO.LOW)
+        GPIO.output(self.electroiman, GPIO.LOW)  
+        GPIO.output(self.semaforo, GPIO.HIGH)
+        GPIO.output(self.flecha_verde, GPIO.HIGH)
+        GPIO.output(self.cruz_roja, GPIO.LOW)
          
     def testArrow(self):
         GPIO.output(self.semaforo, GPIO.LOW)
